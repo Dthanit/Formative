8308cc83c00afc219424c85a714d4fa310560580
Dthanit <43135317+Dthanit@users.noreply.github.com>
2019-01-20 02:13:45 +0700

    Update Formative.java

--- Formative.java
+++ Formative.java
@@ -4,27 +4,25 @@ public class Formative
 {
 	public static void main(String[]args)
 	{
-
-Person p = new Person(name,age);
-String name = "Hero";
-int age = 43;
-System.out.println("Name is and "+name+" Age is "+age);
-Student s = new Student();
-name = "Hero";
-age = 43;
-System.out.println("Name is and "+name+" Age is "+age+"Student ID is "+studentId);
+           Person p = new Person(name,age);
+           String name = "Hero";
+           int age = 43;
+           System.out.println("Name is and "+name+" Age is "+age);
+           Student s = new Student();
+           name = "Hero";
+           age = 43;
+           System.out.println("Name is and "+name+" Age is "+age+"Student ID is "+studentId);
 	}
 class Person
-{
-private String name;
-int age;
-}
+  {
+        private String name;
+        int age;
+  }
 class Student extends Person
-{
-private String name;
-int age;
-private int studentId;
-studentId = 1007089;
-
-}
+   {
+        private String name;
+        int age;
+        private int studentId;
+        studentId = 1007089;
+   }
 }
